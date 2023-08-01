dc9ec41d7db4f83114dc625d1282cbdcecc3051f
Afnan153 <140311940+Afnan153@users.noreply.github.com>
2023-08-02 00:47:13 +0300

    Add files via upload

---
+++ 3LAB/3LAB/_LABApp.swift
@@ -0,0 +1,17 @@
+//
+//  _LABApp.swift
+//  3LAB
+//
+//  Created by afnan althobaiti on 15/01/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct _LABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
